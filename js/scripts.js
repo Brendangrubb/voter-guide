55cbe6e36d868ca95f46fc3386332941fab35a1e
brendan grubb <brendangrubb@gmail.com>
2017-01-12 10:10:48 -0800

    add if else script to show proper page to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
+  var age = parseInt(prompt("What is your age?"));
+
+  if (age > 21) {
+    $("#adults").show();
+    } else {
+      $("#kids").show();
+    };
+
+
 
 });
